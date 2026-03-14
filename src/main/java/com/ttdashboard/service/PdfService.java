838831d88aa7a1f02f87b8739b6ad1551b85f997
dharanisri <23eg105v27@anurag.edu.in>
2026-03-14 08:45:06 +0530

    fix pdf build error

--- src/main/java/com/ttdashboard/service/PdfService.java
+++ src/main/java/com/ttdashboard/service/PdfService.java
@@ -1,15 +1,15 @@
 package com.ttdashboard.service;
 
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.Element;
-import com.lowagie.text.Font;
-import com.lowagie.text.FontFactory;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.Phrase;
-import com.lowagie.text.pdf.PdfPCell;
-import com.lowagie.text.pdf.PdfPTable;
-import com.lowagie.text.pdf.PdfWriter;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.ttdashboard.entity.Project;
 import java.io.ByteArrayOutputStream;
 import java.time.LocalDate;
